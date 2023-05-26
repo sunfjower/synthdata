8410e70a02af1cd20f31231a9497caa5d4adeae5
sunfjower <peremena.work@gmail.com>
2023-05-26 20:54:20 +0300

    Separated backend related functions from the site.js file into a data_preset.js

---
+++ wwwroot/js/data_preset.js
@@ -0,0 +1,64 @@
+﻿function collectBodyData() {
+    let fieldNameArray = [];
+    let fieldTypeArray = [];
+    let inputs = document.getElementsByName("fieldName");
+    let selects = document.getElementsByName("fieldType");
+    let fileFormat = document.getElementsByName("fileFormat");
+
+
+    for (i = 0; i < inputs.length; i++) {
+        fieldNameArray.push(inputs[i].value)
+        fieldTypeArray.push(selects[i].value)
+    }
+
+    let jsonBodyData = JSON.stringify({
+        names: fieldNameArray.join(),
+        types: fieldTypeArray.join(),
+        format: fileFormat[0].value
+    });
+
+    let obj = JSON.parse(jsonBodyData);
+
+    return obj;
+}
+
+function requestDataGeneration() {
+    let bodyData = collectBodyData();
+
+    console.log(bodyData);
+
+    $.ajax({
+        type: "GET",
+        url: "/Generator/GetAll",
+        data: bodyData,
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            console.log(data);
+
+            var link = document.createElement('a');
+            var url = URL.createObjectURL(data);
+
+            if (data.type === "text/plain") {
+                link.setAttribute('download', "synthdata.json");
+            }
+            else {
+                link.setAttribute('download', "synthdata");
+            }
+
+            link.setAttribute('href', url);
+            link.style.display = 'none';
+
+            document.body.appendChild(link);
+
+            link.click();
+
+            document.body.removeChild(link);
+        },
+        error: function (error) {
+            alert("Something goin wrong:" + error.fail);
+            console.log(error);
+        }
+    });
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,18 +1,17 @@
-﻿$(document).ready(function ()
-{
+﻿$(document).ready(function() {
     // New field
     let select = document.getElementsByName("fieldType");
     setSelectionOptions(select);
 });
 
-function toggleMenu() {
+function toggleMenuClick() {
     let toggleBtnIcon = document.querySelector('.toggle_btn i')
     let dropdownMenu = document.querySelector('.dropdown_menu')
 
     dropdownMenu.classList.toggle('open');
     let isOpen = dropdownMenu.classList.contains('open');
 
-    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';    
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
 
 function setSelectionOptions(element) {
@@ -21,7 +20,7 @@ function setSelectionOptions(element) {
         2: "Name",
         3: "Surname",
     };
-    
+
     for (let i = 0; i <= Object.keys(Categories).length; i++) {
         let option = document.createElement("option");
 
@@ -105,70 +104,7 @@ function removeTableField(element) {
     tr.remove();
 }
 
-function collectBodyData() {
-    let fieldNameArray = [];
-    let fieldTypeArray = [];
-    let inputs = document.getElementsByName("fieldName");
-    let selects = document.getElementsByName("fieldType");
-    let fileFormat = document.getElementsByName("fileFormat");
-    
 
-    for (i = 0; i < inputs.length; i++) {
-        fieldNameArray.push(inputs[i].value)
-        fieldTypeArray.push(selects[i].value)
-    }
-
-    let jsonBodyData = JSON.stringify({
-        names: fieldNameArray.join(),
-        types: fieldTypeArray.join(),
-        format: fileFormat[0].value
-    });
-
-    let obj = JSON.parse(jsonBodyData);
-
-    return obj;
-}
-
-function requestDataGeneration() {
-    let bodyData = collectBodyData();
-    
-    console.log(bodyData);
-
-    $.ajax({
-        type: "GET",
-        url: "/Generator/GetAll",
-        data: bodyData,
-        xhrFields: {
-            responseType: 'blob'
-        },
-        success: function (data) {
-            console.log(data);  
-
-            var link = document.createElement('a');
-            var url = URL.createObjectURL(data);
-
-            if (data.type === "text/plain") {
-                link.setAttribute('download', "synthdata.json");
-            }
-            else {
-                link.setAttribute('download', "synthdata");
-            }
-
-            link.setAttribute('href', url);
-            link.style.display = 'none';
-
-            document.body.appendChild(link);
-
-            link.click();
-
-            document.body.removeChild(link);
-        },
-        error: function (error) {
-            alert("Something goin wrong:" + error.fail);
-            console.log(error);
-        }
-    });
-}
 
 
 
